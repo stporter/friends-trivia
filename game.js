a30f59b3f99322ca6a254132438abd3e3a838b78
stporter <stporter2@gmail.com>
2021-12-17 06:22:17 -0500

    js updates

---
+++ game.js
@@ -0,0 +1,19 @@
+// const scoreText = document.querySelector('#score');
+// const questionText = document.querySelector('#question');
+// const answerOptions = document.querySelector('.answer-buttons');
+// const answerSelect = document.querySelector();
+
+// function currentQuestionText(question) {
+// 	ques
+// }
+
+// let questionScore = 0;
+// let currentQuestion = 1;
+
+const questionText = document.querySelector('#question');
+const scoreText = document.querySelector('#score');
+const answerSelect = document.querySelector('.answer-buttons');
+const buttonOne = document.querySelector('#btn1');
+const buttonTwo = document.querySelector('#btn2');
+const buttonThree = document.querySelector('#btn3');
+const buttonFour = document.querySelector('#btn4');
